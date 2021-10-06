f50ff04957042372d589698685c1f4b03a17fba1
Rob L Olson <rob.louis@gmail.com>
2021-10-05 23:48:54 -0800

    improve python profile

--- profile.py
+++ profile.py
@@ -1,5 +1,14 @@
 # Robert Olson
 # python interpreter profile
 
-
 from pprint import pprint as pp
+import rich.traceback
+import os
+import sys
+
+# automatically pretty print objects in terminal
+sys.displayhook = lambda x: exec(['_=x; pp(x)','pass'][x is None])
+
+rich.traceback.install(show_locals=True)
+
+os.environ["PYTHONBREAKPOINT"] = "ipdb.set_trace"
