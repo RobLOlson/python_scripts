4520ab9ce319f835e242844091fbed86d02324be
Rob L Olson <rob.louis@gmail.com>
2021-10-08 12:37:19 -0800

    add a unary function composition utility

--- profile.py
+++ profile.py
@@ -1,11 +1,13 @@
 # Robert Olson
 # python interpreter profile
 
-from pprint import PrettyPrinter
 import rich.traceback
 import os
 import sys
 
+from pprint import PrettyPrinter
+
+from functools import reduce
 pp = PrettyPrinter(sort_dicts=False, underscore_numbers=True).pprint
 
 # automatically pretty print objects in terminal
@@ -14,3 +16,7 @@ sys.displayhook = lambda x: exec(['_=x; pp(x)','pass'][x is None])
 rich.traceback.install(show_locals=True)
 
 os.environ["PYTHONBREAKPOINT"] = "ipdb.set_trace"
+
+def compose(*functions):
+    """Compose multiple unary functions.  E.g., compose(plus_2, times_2, minus_2)"""
+    return reduce(lambda f, g: lambda x: g(f(x)), functions)
