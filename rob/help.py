cc626c5f4f5d7041c94e84ca317b5c65c0e4a1aa
Rob L Olson <rob.louis@gmail.com>
2022-03-06 21:22:43 -0900

    include poetry build version in help.py

--- rob/help.py
+++ rob/help.py
@@ -2,17 +2,22 @@ import glob
 
 from pathlib import Path
 
-breakpoint()
+import toml, rich
+
 
 _EXCLUSIONS = ["__init__"]
+tom = toml.load("pyproject.toml")
+version = tom["tool"]["poetry"]["version"]
+package_name = tom["tool"]["poetry"]["name"]
 
-
-print("Available scripts (pass -h for CLI usage):")
+rich.print(
+    f"[red]Rob's Utility Scripts[/red]\n[green]Version: {version}[/green]\n\nAvailable scripts (pass -h for CLI usage):"
+)
 print(
     "\n".join(
         [
             Path(elem).stem
-            for elem in glob.glob("rob/*.py")
+            for elem in glob.glob("*.py")
             if Path(elem).stem not in _EXCLUSIONS
         ]
     )
