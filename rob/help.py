570f1e3fd34c7d473f1b111e8033b0d1ed28958b
Rob L Olson <rob.louis@gmail.com>
2022-03-06 20:28:15 -0900

    make help.py dynamic

--- rob/help.py
+++ rob/help.py
@@ -1,2 +1,19 @@
-print("Available scripts:")
-print("\nffmpeg_concat")
+import glob
+
+from pathlib import Path
+
+breakpoint()
+
+_EXCLUSIONS = ["__init__"]
+
+
+print("Available scripts (pass -h for CLI usage):")
+print(
+    "\n".join(
+        [
+            Path(elem).stem
+            for elem in glob.glob("rob/*.py")
+            if Path(elem).stem not in _EXCLUSIONS
+        ]
+    )
+)
