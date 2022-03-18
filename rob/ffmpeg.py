f0cc2b6142a83e6a988bd5aeab2b27ed485f62a8
Rob L Olson <rob.louis@gmail.com>
2022-03-17 17:07:38 -0800

    add --command option to ffmpeg.py (for building .ps1 file)

--- rob/ffmpeg.py
+++ rob/ffmpeg.py
@@ -23,7 +23,7 @@ my_parser = argparse.ArgumentParser(
     prog=sys.argv[0],
     allow_abbrev=True,
     add_help=True,
-    description="Concatenates audio files using ffmpeg.",
+    description="Concatenates audio files to m4b using ffmpeg.",
     epilog="(C) Rob",
 )
 
@@ -37,7 +37,7 @@ my_parser.add_argument(
     choices=[".mp3", ".wav", ".ogg"],
     action="store",
     type=str,
-    help="the filetype to concatenate, e.g., '.mp3'",
+    help="the filetype to concatenate (valid options: '.mp3', '.ogg' or '.wav')",
 )
 
 my_parser.add_argument(
@@ -65,20 +65,32 @@ my_parser.add_argument(
     "-n",
     "--number",
     metavar="number",
-    nargs=1,
-    default=[0],
+    default=0,
     action="store",
     type=int,
     help="the number of folders to limit execution on",
 )
 
-my_parser.add_argument('-o',
-                       '--overwrite',
+my_parser.add_argument(
+    "-s",
+    "--start",
+    metavar="start_number",
+    default=0,
+    action="store",
+    type=int,
+    help="start execution on Nth folder (ignoring previous)",
+)
+
+my_parser.add_argument('--safe',
                        action='store_true',
-                       help='force ffmpeg to overwrite existing files')
+                       help='force ffmpeg to ask for overwrite permissions')
+
+my_parser.add_argument('--command',
+                       action='store_true',
+                       help='Only print the corresponding ffmpeg command(s)')
 
 my_parser.add_argument('-i',
-                       '--interactive',
+                       '--interact',
                        action='store_true',
                        help='supply arguments manually via prompt')
 
@@ -94,9 +106,45 @@ _PROMPT = f"rob.{pathlib.Path(__file__).stem}> "
 
 _PATH = pathlib.Path(_ARGS.path)
 _FILETYPE = _ARGS.filetype
-_OVERWRITE = _ARGS.overwrite
+_SAFE = _ARGS.safe
 _CPUS = _ARGS.cpus
+_COMMAND = _ARGS.command
+_TEMP_FOLDER = pathlib.Path(f"{appdirs.user_data_dir()}") / "robolson" / "ffmpeg" / "temp"
+_COMMAND_FILE = pathlib.Path(os.getcwd()) / "ffmpeg_commands.ps1"
 
+def command_only(folder):
+    os.chdir(folder)
+
+    mp3s = glob.glob(f"*{_FILETYPE}")
+    mp3s = [pathlib.Path(elem) for elem in mp3s if elem[0] != '~']
+
+    if not mp3s:
+        return
+
+    concated = "|".join([str(elem.absolute()) for elem in mp3s])
+    command = [
+        "ffmpeg",
+        "-i",
+        f"'concat:{concated}'",
+        "-movflags", # Carry metadata over
+        "use_metadata_tags",
+        "-c:a", # audio cocec
+        "aac",
+        "-b:a", # bitrate
+        "64k",
+        "-c:v",
+        "copy",
+        f"{pathlib.Path(mp3s[0]).stem}.m4b",
+        "-y",
+    ]
+
+
+    with open(_COMMAND_FILE, "a") as fp:
+        fp.write(" ".join(command)+"\n")
+
+    rich.print("[green]"+" ".join(command))
+
+    return
 
 def spin_up(folder):
     """Spin up an ffmpeg process in target folder.
@@ -107,42 +155,77 @@ def spin_up(folder):
 
     os.chdir(folder)
 
-    global _OVERWRITE
+    global _SAFE
 
     mp3s = glob.glob(f"*{_FILETYPE}")
-    mp3s = [elem for elem in mp3s if elem[0] != '~']
+    mp3s = [pathlib.Path(elem) for elem in mp3s if elem[0] != '~']
 
 
     if not mp3s:
         return
 
-    bit_rate = mediainfo(mp3s[0])['bit_rate']
-
-    # Merge audio files
-    combined = pydub.AudioSegment.empty()
-
-    match _FILETYPE:
-        case ".mp3":
-            for file in track(mp3s):
-                combined += pydub.AudioSegment.from_mp3(file)
-
-        case ".wav":
-            for file in track(mp3s):
-                combined += pydub.AudioSegment.from_wav(file)
-
-        case ".ogg":
-            for file in track(mp3s):
-                combined += pydub.AudioSegment.from_ogg(file)
+    concated = "|".join([str(elem.absolute()) for elem in mp3s])
 
     salt = ""
     if _ARGS.local:
         salt = "".join(random.choices("abcdefghijklmnopqrstuvwxyz", k=10))
-        os.makedirs(pathlib.Path(f"{appdirs.user_data_dir()}") / "robolson" / "ffmpeg" / "temp", exist_ok=True)
-        _temp_file = pathlib.Path(f"{appdirs.user_data_dir()}") / "robolson" / "ffmpeg" / "temp"/ f"{salt}~{pathlib.Path(mp3s[0]).stem}-full{_FILETYPE}"
+        os.makedirs(_TEMP_FOLDER, exist_ok=True)
+        _temp_file = _TEMP_FOLDER / f"{salt}~{pathlib.Path(mp3s[0]).stem}-full{_FILETYPE}"
     else:
         _temp_file = f"~{pathlib.Path(mp3s[0]).stem}-full{_FILETYPE}"
 
-    combined.export(_temp_file, tags={"test":"TEST"}, bitrate=bit_rate)
+
+    command = [
+        "ffmpeg",
+        "-i",
+        str(_temp_file), # f"~{pathlib.Path(mp3s[0]).stem}-full{_FILETYPE}",
+        "-movflags", # Carry metadata over
+        "use_metadata_tags",
+        "-c:a", # audio cocec
+        "aac",
+        "-b:a", # bitrate
+        "64k",
+        "-c:v",
+        "copy",
+        f"{pathlib.Path(mp3s[0]).stem}.m4b",
+        "-y"
+    ]
+
+    if _SAFE:
+        command.remove('-y')
+
+    bit_rate = mediainfo(mp3s[0])['bit_rate']
+
+    # Merge audio files
+
+    if len(mp3s)==1:
+        combined = pydub.AudioSegment.from_mp3(mp3s[0])
+
+    else:
+        combined = pydub.AudioSegment.empty()
+        match _FILETYPE:
+            case ".mp3":
+                for file in track(mp3s):
+                    combined += pydub.AudioSegment.from_mp3(file)
+                    # combined += pydub.AudioSegment.from_file(file, "mp3")
+
+            case ".wav":
+                for file in track(mp3s):
+                    combined += pydub.AudioSegment.from_wav(file)
+
+            case ".ogg":
+                for file in track(mp3s):
+                    combined += pydub.AudioSegment.from_ogg(file)
+
+    rich.print(f"[yellow]Writing temp file ({pathlib.Path(_temp_file).absolute()}) to disk.")
+    # combined.export(_temp_file, tags={"test":"TEST"}, bitrate=bit_rate)
+    combined.export(
+    _temp_file,
+    format="ipod",
+    # bitrate=bit_rate,
+    parameters=["-movflags", "use_metadata_tags", "-c:a", "aac", "-c:v", "copy", "-b:a", "64k"],
+)
+
     # End of Merge audio files
 
     count = 0
@@ -157,35 +240,25 @@ def spin_up(folder):
     rich.print(f"\n[green]Merged file ({pathlib.Path(_temp_file).absolute()}) created.[/green]\n")
 
     # Convert audio files
-    command = [
-        "ffmpeg",
-        "-i",
-        str(_temp_file), # f"~{pathlib.Path(mp3s[0]).stem}-full{_FILETYPE}",
-        "-movflags", # Carry metadata over
-        "use_metadata_tags",
-        "-c:a", # audio cocec
-        "aac",
-        "-b:a", # bitrate
-        "64k",
-        "-c:v",
-        "copy",
-        f"{pathlib.Path(mp3s[0]).stem}.m4b",
-    ]
 
     rich.print("\nRunning the following command:\n[yellow]" + " ".join(command) + "[/yellow]\n")
     subprocess.run(command, shell=True)
     # End of Convert audio files
 
     if salt:
+        rich.print(f"[red]Removing temp file ({pathlib.Path(_temp_file).absolute()}).")
         os.remove(_temp_file)
 
     rich.print(f"[green]Finished {folder}.\n")
 
+    return
+
 def interact():
     global _PATH
     global _FILETYPE
-    global _OVERWRITE
+    global _SAFE
     global _CPUS
+    global _COMMAND
 
     choice = input(f"Enter path to work on [default: {_PATH.absolute()}].\n{_PROMPT}")
     if choice:
@@ -193,21 +266,20 @@ def interact():
 
     rich.print(f"[yellow]Working on '{_PATH.absolute()}'.\n")
 
-    choice = input(f"Automatically overwrite existing files [default: {_OVERWRITE}]? y/n\n{_PROMPT}")
+
+    choice = input(f"Automatically overwrite existing files [default: {_SAFE}]? y/n\n{_PROMPT}")
     if choice in ['y', "Y", 'yes', "YES"]:
-        _ARGS.overwrite = True
-        _OVERWRITE = True
+        _SAFE = False
 
     elif not choice:
         pass
     else:
-        _ARGS.overwrite = False
-        _OVERWRITE = False
+        _SAFE = True
 
-    if _ARGS.overwrite:
-        rich.print("[yellow]Overwriting...\n")
+    if not _SAFE:
+        rich.print("[red]Overwriting...\n")
     else:
-        rich.print("[red]NOT[/red] [yellow]overwriting...\n")
+        rich.print("[green]NOT overwriting...\n")
 
     choice = input(f"Which file type to work on [default: {_FILETYPE}]?\n 1.) *.mp3\n 2.) *.wav\n 3.) *.ogg\n{_PROMPT}")
     match choice:
@@ -225,6 +297,53 @@ def interact():
 
     rich.print(f"[yellow]Working on {_FILETYPE}'s.\n")
 
+    folders = set()
+    os.chdir(_PATH)
+    for file in glob.glob(f"**/*{_FILETYPE}", recursive=True):
+        if _SAFE:
+            try:
+                os.remove(pathlib.Path(f"{pathlib.Path(file).parent}/{pathlib.Path(file).stem}.m4b"))
+            except FileNotFoundError as e:
+                pass
+        target = pathlib.Path(file).absolute()
+        folders.add(target.parent)
+
+    all_folders = sorted(list(folders))
+    folders = all_folders[:]
+
+    final_choice = True
+    while final_choice:
+        rich.print(f"Found folders with {_FILETYPE}'s:")
+        for count, folder in enumerate(all_folders):
+            if folder in folders:
+                rich.print(f"[green]{count+1}.) {folder}")
+            else:
+                rich.print(f"[red]{count+1}.) {folder}")
+
+        choice = input(f"Toggle execution of folders by number or press Enter to continue.\n{_PROMPT}")
+        try:
+            choice = int(choice)
+            if choice <= len(all_folders):
+                if all_folders[choice-1] in folders:
+                    folders.remove(all_folders[choice-1])
+                else:
+                    folders.insert(choice-1, all_folders[choice-1])
+            else:
+                rich.print(f"Found folders with {_FILETYPE}'s:")
+                for count, folder in enumerate(all_folders):
+                    if folder in folders:
+                        rich.print(f"[green]{count+1}.) {folder}")
+                    else:
+                        rich.print(f"[red]{count+1}.) {folder}")
+                final_choice = input(f"Continue? (y/n)\n{_PROMPT}")
+                if final_choice not in ["n", "no", "N", "No", "NO"]:
+                    final_choice = False
+
+        except (SyntaxError, ValueError):
+            final_choice = input(f"Continue? (y/n)\n{_PROMPT}")
+            if final_choice not in ["n", "no", "N", "No", "NO"]:
+                final_choice = False
+
     choice = input(f"How many CPU cores to use [default: {_CPUS}]?\n{_PROMPT}")
     while True:
         try:
@@ -239,38 +358,65 @@ def interact():
 
     rich.print(f"[yellow]Using {_CPUS} core{'s' if _CPUS>1 else ''}.\n")
 
+    choice = input(f"(1) Execute commands or (2) Generate command file [default {'1' if _COMMAND else '1'}]?\n{_PROMPT}")
+    match choice:
+        case '1':
+            _COMMAND = False
+        case '2':
+            _COMMAND = True
+            open(_COMMAND_FILE, "w").close()
+        case _:
+            pass
+
+    return folders
+
 def main():
     folders = set()
 
-    global _OVERWRITE
+    global _SAFE
+
+    if _COMMAND:
+        open(_COMMAND_FILE, "w").close()
 
     if _ARGS.interactive:
-        interact()
+        folders = interact()
 
+    else:
+        os.chdir(_PATH)
+        for file in glob.glob(f"**/*{_FILETYPE}", recursive=True):
+            if _SAFE:
+                try:
+                    os.remove(pathlib.Path(f"{pathlib.Path(file).parent}/{pathlib.Path(file).stem}.m4b"))
+                except FileNotFoundError as e:
+                    pass
+            target = pathlib.Path(file).absolute()
+            folders.add(target.parent)
 
-    os.chdir(_PATH)
-    for file in glob.glob(f"**/*{_FILETYPE}", recursive=True):
-        if _OVERWRITE:
-            try:
-                os.remove(pathlib.Path(f"{pathlib.Path(file).parent}/{pathlib.Path(file).stem}.m4b"))
-            except FileNotFoundError as e:
+        folders = sorted(list(folders))
+
+        # optionally slice the list of folders
+        match (bool(_ARGS.number), bool(_ARGS.start)):
+            case (True, False):
+                folders = folders[0:_ARGS.number]
+            case (False, True):
+                folders = folders[_ARGS.start-1:]
+            case (True, True):
+                folders = folders[_ARGS.start-1:_ARGS.start-1+_ARGS.number]
+            case _:
                 pass
-        target = pathlib.Path(file).absolute()
-        folders.add(target.parent)
-
-    folders = sorted(list(folders))
-
-    if _ARGS.number[0]:
-        folders = folders[:_ARGS.number[0]]
 
     input(
-        f"Executing on \n * "
+        f"{'Compiling' if _COMMAND else 'Executing'} on \n * "
         + "\n * ".join([str(folder) for folder in folders])
-        + ". \nOk?"
+        + f". \n Using {_CPUS} core. Ok?\n{_PROMPT}"
     )
 
-    with multiprocessing.Pool(_CPUS) as pool:
-        pool.map_async(spin_up, folders).get()
+    if _COMMAND:
+        with multiprocessing.Pool(_CPUS) as pool:
+            pool.map_async(command_only, folders).get()
+    else:
+        with multiprocessing.Pool(_CPUS) as pool:
+            pool.map_async(spin_up, folders).get()
 
 if __name__ == "__main__":
     main()
