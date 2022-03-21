66b99cb7caa7506c663c64772686f441d69a9edf
Rob L Olson <rob.louis@gmail.com>
2022-03-21 14:26:26 -0800

    invert .gitignore

--- rob/help.py
+++ rob/help.py
@@ -2,24 +2,41 @@ import glob
 
 from pathlib import Path
 
-import toml, rich
+import toml, rich, sys
 
 
-_EXCLUSIONS = ["__init__"]
+_EXCLUSIONS = ["__init__", "__main__"]
 
-tom = toml.load(Path(__file__).parent / "pyproject.toml")
-version = tom["tool"]["poetry"]["version"]
-package_name = tom["tool"]["poetry"]["name"]
 
-rich.print(
-    f"[red]Rob's Utility Scripts[/red]\n[green]Version: {version}[/green]\n\nAvailable scripts (pass -h for CLI usage):"
-)
-print(
-    "\n".join(
-        [
-            Path(elem).stem
-            for elem in glob.glob(f"{Path(__file__).parent}/*.py")
-            if Path(elem).stem not in _EXCLUSIONS
-        ]
+def main():
+
+    tom = toml.load(Path(__file__).parent / "pyproject.toml")
+    version = tom["tool"]["poetry"]["version"]
+    package_name = tom["tool"]["poetry"]["name"]
+
+    rich.print(
+        f"[red]Rob's Utility Scripts[/red]\n[green]Version: {version}[/green]\n\nAvailable scripts (pass -h for CLI usage):"
     )
-)
+
+    scripts = []
+    # breakpoint()
+    for elem in glob.glob(f"{Path(__file__).parent}/*.py"):
+        if Path(elem).stem not in _EXCLUSIONS:
+            scripts.append("rob." + Path(elem).stem)
+
+    print("\n".join(elem for elem in scripts))
+    sys.stdout = None
+    sys.stderr = None
+    # print(
+    #     "\n".join(
+    #         [
+    #             "rob." + Path(elem).stem
+    #             for elem in glob.glob(f"{Path(__file__).parent}/*.py")
+    #             if Path(elem).stem not in _EXCLUSIONS
+    #         ]
+    #     )
+    # )
+
+
+if __name__ == "__main__":
+    main()
