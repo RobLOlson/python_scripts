9596bf6542951bb126e6bd503a0194b2e8d4ab61
Rob L Olson <rob.louis@gmail.com>
2022-07-06 20:26:57 -0800

    revert redengine refactor
    (it used up a significant degree of CPU for some reason)

--- rob/tick.py
+++ rob/tick.py
@@ -6,7 +6,6 @@ import time
 from pathlib import Path
 
 import appdirs  # type: ignore[import]
-from redengine import RedEngine
 
 from .parser.tick_parser import tick_parser  # type: ignore[import]
 from .ticktick.api import TickTickClient
@@ -17,7 +16,6 @@ from .ticktick.oauth2 import OAuth2
 # for debugging the daemon
 # import logging
 
-repeater = RedEngine()
 
 # from .loggers.tick_logger import log, StreamToLogger
 
@@ -62,18 +60,6 @@ if not _TASK_FILE:
         os.makedirs(_TASK_FILE.parent, exist_ok=True)
         open(f"{_TASK_FILE}", "w").close()
 
-_AUTH_CLIENT = OAuth2(
-    client_id=_CLIENT_ID,
-    client_secret=_CLIENT_SECRET,
-    redirect_uri="http://127.0.0.1:8080",
-    cache_path=str(_TOKEN_FILE),
-    check_cache=True,
-)
-
-tick_client = TickTickClient(
-    username=_TICKTICK_USERNAME, password=_TICKTICK_PASSWORD, oauth=_AUTH_CLIENT
-)
-
 
 if args.get:
     last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(_TASK_FILE))
@@ -84,8 +70,7 @@ if args.get:
     exit(0)
 
 
-def get_due() -> list[str]:
-    # global tick_client
+def get_due(tick_client) -> list[str]:
     today_str = datetime.datetime.today().isoformat("!")
     today_date = today_str
     today_str = today_str.split("!")[0]
@@ -103,60 +88,6 @@ def get_due() -> list[str]:
     return due
 
 
-@repeater.task("every 5 minutes")
-def update():
-    due = get_due()
-    # print(f"DUE:{[elem['title'] for elem in due]}")
-
-    # Create Overdue Tasks in "me" list for Pavlok
-    for task in due:
-        # Do not trigger during sleeping hours
-        if not 8 < datetime.datetime.now().hour < 20:
-            continue
-
-        due_date = datetime.datetime.fromisoformat(task["dueDate"][:-5])
-        due_diff = datetime.datetime.now() - due_date
-
-        if due_diff.days > 0 and 0.99**due_diff.days < random.random():
-
-            if task["projectId"] == "54384b29b84562b41688e91a":
-                # If task in Joes List, add to Joes Pavlok List
-                new_projectId = "8999486ebf8fc984f752dd46"
-
-            elif task["projectId"] == "af22459697ae1243ca23f4d9":
-                # If task in Eryns List, add to Eryns Pavlok List
-                new_projectId = "88ba4b50ab907613d84d0cb6"
-
-            else:
-                # Add to Robs Pavlok List
-                new_projectId = "611479cafba2c1d019f96b45"
-
-            new_due = datetime.datetime.fromordinal(
-                datetime.datetime.today().toordinal()
-            )
-            new_due = new_due + datetime.timedelta(hours=20)
-            new_due = new_due.isoformat("T") + "+0000"
-            new_task = tick_client.task.builder(
-                title=f"Overdue({due_diff.days}): {task['title']}",
-                dueDate=new_due,
-                content="Created for Pavlok",
-                priority=5,
-                projectId=new_projectId,
-            )
-            new_task["dueDate"] = new_due
-            tick_client.task.create(new_task)
-
-    with open(f"{_TASK_FILE}", "w") as fp:
-        if due:
-            # write name of due task to file if it is assigned to inbox
-            fp.write("\n".join([elem["title"] for elem in due if elem["projectId"] == "inbox115493726"]))  # type: ignore[index]
-        else:
-            fp.write("")
-
-    # log.info("syncing with ticktick servers")
-    tick_client.sync()
-
-
 def main() -> None:
 
     global args
@@ -168,6 +99,18 @@ def main() -> None:
     # stderr_logger = logging.getLogger("STDERR")
     # sl = StreamToLogger(stderr_logger, logging.ERROR)
     # sys.stderr = sl
+
+    auth_client = OAuth2(
+        client_id=_CLIENT_ID,
+        client_secret=_CLIENT_SECRET,
+        redirect_uri="http://127.0.0.1:8080",
+        cache_path=str(_TOKEN_FILE),
+        check_cache=True,
+    )
+
+    tick_client = TickTickClient(
+        username=_TICKTICK_USERNAME, password=_TICKTICK_PASSWORD, oauth=auth_client
+    )
 
     if args.update:
         due = get_due(tick_client)
@@ -184,7 +127,58 @@ def main() -> None:
         subprocess.Popen(["pyw", "-m", "rob.tick"])
         exit(0)
 
-    repeater.run()
+    while True:
+        due = get_due(tick_client)
+
+        # Create Overdue Tasks in "me" list for Pavlok
+        for task in due:
+            # Do not trigger during sleeping hours
+            if not 8 < datetime.datetime.now().hour < 20:
+                continue
+
+            due_date = datetime.datetime.fromisoformat(task["dueDate"][:-5])
+            due_diff = datetime.datetime.now() - due_date
+
+            if due_diff.days > 0 and 0.99**due_diff.days < random.random():
+
+                if task["projectId"] == "54384b29b84562b41688e91a":
+                    # If task in Joes List, add to Joes Pavlok List
+                    new_projectId = "8999486ebf8fc984f752dd46"
+
+                elif task["projectId"] == "af22459697ae1243ca23f4d9":
+                    # If task in Eryns List, add to Eryns Pavlok List
+                    new_projectId = "88ba4b50ab907613d84d0cb6"
+
+                else:
+                    # Add to Robs Pavlok List
+                    new_projectId = "611479cafba2c1d019f96b45"
+
+                new_due = datetime.datetime.fromordinal(
+                    datetime.datetime.today().toordinal()
+                )
+                new_due = new_due + datetime.timedelta(hours=20)
+                new_due = new_due.isoformat("T") + "+0000"
+                new_task = tick_client.task.builder(
+                    title=f"Overdue({due_diff.days}): {task['title']}",
+                    dueDate=new_due,
+                    content="Created for Pavlok",
+                    priority=5,
+                    projectId=new_projectId,
+                )
+                new_task["dueDate"] = new_due
+                tick_client.task.create(new_task)
+
+        with open(f"{_TASK_FILE}", "w") as fp:
+            if due:
+                # write name of due task to file if it is assigned to inbox
+                fp.write("\n".join([elem["title"] for elem in due if elem["projectId"] == "inbox115493726"]))  # type: ignore[index]
+            else:
+                fp.write("")
+
+        # log.info("syncing with ticktick servers")
+        tick_client.sync()
+
+        time.sleep(299)
 
 
 if __name__ == "__main__":
