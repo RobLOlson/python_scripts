825350c6375f59add104a1ce7e4222c1779a2545
Rob Olson <rob.louis@gmail.com>
2024-11-17 12:44:37 -0900

    add form_from_dict() to query.py

--- rob/utilities/query.py
+++ rob/utilities/query.py
@@ -14,7 +14,7 @@ _CLEAR_RIGHT = "\033[K"
 
 
 def approve_list(
-    target: list[Any], preamble: str = "", repr_func=None, maximum: int | None = None, yes_all:bool=False
+    target: list[Any], repr_func=None, maximum: int | None = None, preamble: bool | None = None , default_yes:bool=False
 ) -> list[Any]:
     """Interactively approves items from a list using keyboard input.
 
@@ -34,28 +34,37 @@ def approve_list(
         A new list containing only the approved items from the original list.
     """
 
-    if maximum and maximum < 1:
+    if maximum != None and maximum < 1:
         return []
 
     if not target:
         return []
-    if yes_all:
-        approved_targets = list(range(1, len(target)+1))
+
+    if preamble == None:
+        if maximum == 1:
+            preamble = False
+        else:
+            preamble = True
+
+    if default_yes:
+        if maximum:
+            approved_targets = [(i) % len(target) + 1 for i in range(len(target)) if i < maximum]
+        else:
+            approved_targets = list(range(1, len(target)+1))
     else:
         approved_targets = []
 
     cursor_index = 0
 
-    long_contents = max(len(str(elem)) for elem in target) > shutil.get_terminal_size().columns
+    long_contents = max([len(str(elem))+8 for elem in target]) > shutil.get_terminal_size().columns
 
-    rich.print(
+    if preamble:
+        rich.print(
         f"""\n[green]Toggle approval with number keys.
 Move cursor with up/down.
 Add/remove approval with left/right.
 Press Enter to continue."""
     )
-    if preamble:
-        rich.print("\n" + preamble)
 
     print("\n" * (len(target)))
     while True:
@@ -70,11 +79,22 @@ Press Enter to continue."""
                 display = item
 
             style = "[green]" if approved_targets.count(index+1) else "[red]"
+            if maximum and maximum == 1:
+                style = "[white]"
             if index == cursor_index:
                 style = "[yellow]"
 
-            print(f'[{'x' if approved_targets.count(index+1) else ' '}]', end="")
-            rich.print(rf" {style}{index+1:02}.) {display}")
+
+            if not maximum or maximum > 1:
+                print(f'[{'x' if approved_targets.count(index+1) else ' '}]', end="")
+                prefix = f"{index+1:02}.) "
+            else:
+                if index == cursor_index:
+                    prefix = " >"
+                else:
+                    prefix = "  "
+
+            rich.print(rf"{style}{prefix}{display}")
 
         choice = readchar.readkey()
         match choice:
@@ -125,11 +145,21 @@ Press Enter to continue."""
                 cursor_index = cursor_index % len(target)
 
             case '\r':
+                if maximum and maximum == 1:
+                    approved_targets = [cursor_index + 1]
+
                 break
+
+            case '\x1b':
+                rich.print("[red]Terminated.", end="")
+                exit(1)
 
     return [elem for i, elem in enumerate(target) if approved_targets.count(i+1)]
 
+def select(target: list[Any], preamble: bool=False, repr_func = None):
+    """Select and return a user-approved element from target list."""
 
+    return approve_list(target, preamble=preamble, repr_func=repr_func, maximum=1)[0]
 
 def approve_dict(
     target: dict[Any, Any], preamble: str = "", repr_func=None, maximum: int | None = None
@@ -151,7 +181,7 @@ def approve_dict(
         A new dictionary containing only the approved entries.  Returns an empty
         dictionary if the input dictionary is empty.
     """
-    if maximum and maximum < 1:
+    if maximum != None and maximum < 1:
         return {}
 
     if not target:
@@ -237,10 +267,14 @@ Press Enter to continue."""
             case '\r':
                 break
 
+            case '\x1b':
+                rich.print("[red]Terminated.", end="")
+                exit(1)
+
     return {elem:target[elem] for i, elem in enumerate(target) if approved_targets.count(i+1)}
 
 
-def linearize_complex_object(object:list|dict, depth:int = 0) -> tuple[Any, type, int]:
+def linearize_complex_object(object:list|dict, depth:int = 0) -> tuple[Any, int, type]:
     linearized = []
     if type(object) == dict:
         keys=object.keys()
@@ -275,30 +309,66 @@ def print_linearized_object(linearized_object):
         print('  '*(line[1]+1) + str(line[0]) + (f" ({line[2]})" if line[2] else ""))
 
 def edit_object(
-    target: list[Any]|dict[Any, Any], preamble: str = "", repr_func=None, maximum: int | None = None
+    target: list[Any]|dict[Any, Any], preamble: str = "", repr_func=None, show_brackets: bool = True, edit_keys:bool = True, dict_inline:bool=False
 ):
 
     target2 = linearize_complex_object(target)
 
     cursor_index = 0
+
+    while True:
+        cursor_index = (cursor_index - 1) % len(target2)
+        if not target2[cursor_index][2]: # [2] is element's type ("brackets" have no type)
+            continue
+        if not edit_keys and target2[(cursor_index + 1) % len(target2)][0] == ":": #[0] is contents
+            continue
+        break
+
     rich.print(
         f"\n[green]Move cursor with up/down. Press right or tab to edit. Press Enter to confirm and commit."
     )
     rich.print(preamble)
 
     long_contents = max(len(str(elem)) for elem in target2) > shutil.get_terminal_size().columns
+    end = ""
 
     print("\n" * (len(target2) + 1))
     while True:
-        if long_contents:
+        if long_contents or not edit_keys:
             print("\033[2J")
         else:
             print((_MOVE_UP + _CLEAR_LINE) * (len(target2)+1))
+        display_string = ""
         for index, item in enumerate(target2):
+            if not show_brackets and not item[2] and str(item[0]) in "]}[{":
+                continue
             if repr_func:
                 display = repr_func(item)
             else:
-                display = "  "*int(item[1]+1) + f"{item[0]}"
+                display = f"{item[0]}"
+
+            if dict_inline:
+                # index ON ':'
+                if item[0]==':' and str(target2[(index+1) % len(target2)][0]) not in '{}[]':
+                    indent = ""
+                    end = " "
+
+                # index AFTER ':'
+                elif str(target2[(index-1) % len(target2)][0]) in ':':
+                    indent = ""
+                    end = "\n"
+
+                # index BEFORE ':'
+                elif target2[(index+1) % len(target2)][0] == ':' or str(target2[(index-1) % len(target2)][0]) == '{':
+                    indent = "  "*int(item[1]+1)
+                    # indent = ""
+                    end = ""
+                else:
+                    indent = "  "*int(item[1]+1)
+                    end = "\n"
+            else:
+                indent = "  "*int(item[1]+1)
+                end = "\n"
 
             style = "[white]"
             if index == cursor_index:
@@ -306,7 +376,11 @@ def edit_object(
                     style = "[green]"
                 else:
                     style = "[red]"
-            rich.print(rf"{style}{display}")
+
+            display_string += rf"{style}{indent}{display}{end}"
+            # rich.print(rf"{style}{indent}{display}{end}", end="")
+
+        rich.print(display_string, end="")
 
         choice = readchar.readkey()
         match choice:
@@ -315,24 +389,58 @@ def edit_object(
                     continue
 
                 modified_text = ""
-                print((_MOVE_UP + _CLEAR_LINE) * (len(target2) + 1))
+                if long_contents or not edit_keys:
+                    print("\033[2J")
+                else:
+                    print((_MOVE_UP + _CLEAR_LINE) * (len(target2)+1))
+                display_string = ""
                 for index, item in enumerate(target2):
+                    if not show_brackets and not item[2] and str(item[0]) in "]}[{":
+                        continue
                     if repr_func:
                         display = repr_func(item)
                     else:
-                        display = "  "*int(item[1]+1) + f"{item[0]}"
+                        display = f"{item[0]}"
+
+                    if dict_inline:
+                        # index ON ':'
+                        if item[0]==':' and str(target2[(index+1) % len(target2)][0]) not in '{}[]':
+                            indent = ""
+                            end = " "
+
+                        # index AFTER ':'
+                        elif str(target2[(index-1) % len(target2)][0]) in ':':
+                            indent = ""
+                            end = "\n"
+
+                        # index BEFORE ':'
+                        elif target2[(index+1) % len(target2)][0] == ':' or str(target2[(index-1) % len(target2)][0]) == '{':
+                            indent = "  "*int(item[1]+1)
+                            # indent = ""
+                            end = ""
+                        else:
+                            indent = "  "*int(item[1]+1)
+                            end = "\n"
+                    else:
+                        indent = "  "*int(item[1]+1)
+                        end = "\n"
 
                     style = "[white]"
                     if index == cursor_index:
-                        style = "[green]"
-                        # print(_SAVE_CURSOR + _MOVE_UP + _CLEAR_LINE)
-                        display = "  "*int(item[1]+1)
-                        modified_text = f"{item[0]}"
+                        edit_prefix = display_string.split("\n")[-1]
+                        edit_line = display_string.count("\n")
+                        if item[2]:
+                            style = "[green]"
+                        else:
+                            style = "[red]"
 
-                    rich.print(rf"{style}{display}")
+                    display_string += rf"{style}{indent}{display}{end}"
+                    # rich.print(rf"{style}{indent}{display}{end}", end="")
 
+                rich.print(display_string, end="")
+                display_height=display_string.count("\n")
                 # Move cursor to target2 line
-                print(_MOVE_UP*(len(target2)-cursor_index), end="")
+                print(_MOVE_UP*(abs(edit_line - display_height)), end="")
 
                 modified_text = str(target2[cursor_index][0])
 
@@ -342,7 +450,12 @@ def edit_object(
                 # Move cursor to start of target2 data
                 print(_MOVE_LEFT*len(modified_text), end="")
 
-                replace = input("  "*int(target2[cursor_index][1]+1))
+                if dict_inline:
+                    rich.print(f"{edit_prefix}", end="")
+                    replace = input()
+
+                else:
+                    replace = input("  "*int(target2[cursor_index][1]+1))
 
                 if replace:
                     target2[cursor_index] = (replace, target2[cursor_index][1], target2[cursor_index][2])
@@ -350,21 +463,39 @@ def edit_object(
                 print(_MOVE_DOWN*(len(target2)-cursor_index))
 
             case "s"|"j"|readchar.key.DOWN:
-                cursor_index = (cursor_index + 1) % len(target2)
-                while not target2[cursor_index][2]:
+                while True:
                     cursor_index = (cursor_index + 1) % len(target2)
+                    if not target2[cursor_index][2]:
+                        continue
+                    if not edit_keys and target2[(cursor_index + 1) % len(target2)][0] == ":":
+                        continue
+                    break
+                # while not target2[cursor_index][2] and True if edit_keys else target2[(cursor_index + 2) % len(target2)][0] != ':':
+                #     cursor_index = (cursor_index + 1) % len(target2)
 
                 # cursor_index = cursor_index % len(target2)
 
             case "w"|"k"|readchar.key.UP:
-                cursor_index = (cursor_index - 1) % len(target2)
-                while not target2[cursor_index][2]:
+                while True:
                     cursor_index = (cursor_index - 1) % len(target2)
+                    if not target2[cursor_index][2]:
+                        continue
+                    if not edit_keys and target2[(cursor_index + 1) % len(target2)][0] == ":":
+                        continue
+                    break
+
+                # cursor_index = (cursor_index - 1) % len(target2)
+                # while not target2[cursor_index][2]:
+                #     cursor_index = (cursor_index - 1) % len(target2)
 
                 # cursor_index = cursor_index % len(target2)
 
             case '\r':
                 break
+
+            case '\x1b':
+                rich.print("[red]Terminated.", end="")
+                exit(1)
 
     return reconstitute_object(target2)
 
@@ -381,7 +512,7 @@ def reconstitute_object(linearized_object):
         if not line[2] and line[0] in ['[', ']', '{', '}'] and line[1] > current_depth:
             highest_indeces = []
             current_depth = line[1]
-        if not line[2] and line[1] == current_depth:
+        if not line[2] and line[0] != ':' and line[1] == current_depth:
             highest_indeces.append(index)
 
     if not highest_indeces:
@@ -449,15 +580,19 @@ def reconstitute_object(linearized_object):
         pre_list.extend(post_list)
         composite = pre_list
     else:
+        # breakpoint()
         raise Exception("Expected '[' or '{'.")
 
     return reconstitute_object(composite)
+
+def form_from_dict(target:dict[str, str|int|float|bool]):
+    return edit_object(target=target, show_brackets=False, edit_keys=False, dict_inline=True)
 
 # print(approve_list([1,2,3,4,5,6,7,8,9,10]))
 # print(approve_dict({'settings_1':True, 'settings_2':False, 'settings_3':False, 'settings_4':False, 'settings_5':False}))
 # print_linearized_object(linearize_complex_object({'a': [1, 2, {'x':'y', 'z':'w'}]}))
 # print_linearized_object(linearize_complex_object([{'settings_1':True, 'settings_2':False}]))
-# print(edit_object([reconstitute_object, 'uno', 'dos', {'aba': [1, 2, {'xeno':'y', 'z':'w'}, reconstitute_object]}]))
-# print(edit_object({'a': {1:2}}))
+# print(edit_object([reconstitute_object, 'uno', 'dos', {'aba': [1, 2, {'xeno':'y', 'z':'w'}, reconstitute_object]}], show_brackets=False, edit_keys=False, dict_inline=True))
+# print(form_from_dict({'a': {1:2, 2:4, 3:6}, 'b':{'option_1': True, 'option_2': False}}))
 
 # print(approve_list(edit_object([False, 'dos', {1:'one', 2:'two', 3:[3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9]}, ['quatro', 'cinco', [1,2,3.5]]])))
