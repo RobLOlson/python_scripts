d533a2e2552dcba1c59bb03b975c07685d386cd7
Rob Olson <rob.louis@gmail.com>
2024-11-17 17:28:11 -0900

    add project.py

---
+++ rob/project.py
@@ -0,0 +1,20 @@
+# to do
+# git
+#   git hooks
+# pytest
+# typer (CLI)
+# logger
+# poetry / PyPi
+# toml config/database
+# other database
+import typer
+
+app = typer.Typer()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    app()
