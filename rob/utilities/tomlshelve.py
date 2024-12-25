9d024a6dc3350b01971727b0032d77316dc76887
Rob Olson <rob.louis@gmail.com>
2024-12-24 21:06:40 -0900

    catch up forgotten updates to tomlshelve.py

--- rob/utilities/tomlshelve.py
+++ rob/utilities/tomlshelve.py
@@ -1,10 +1,11 @@
-import toml
-
 try:
-    from . import tomldict
-except:
-    import tomldict
+    # from . import tomldict
+    from tomldict import TomlDict
+except ImportError:
+    from .tomldict import TomlDict
 
 
-def open(filename):
-    return tomldict.TomlDict(filename)
+def open(filename: str) -> TomlDict:
+    """Takes a .toml and returns a dictionary that reflects its structure and contents."""
+
+    return TomlDict(filename)
